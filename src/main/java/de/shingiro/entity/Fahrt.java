77e958f3db69cd53706d6eb477ae49d1a4527da1
niceGeek <angedesires@gmail.com>
2018-08-29 04:04:53 +0200

    New ride entity

---
+++ src/main/java/de/shingiro/entity/Fahrt.java
@@ -0,0 +1,120 @@
+package de.shingiro.entity;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+/**
+ * Entity class for ride object
+ * 
+ * @author angedesires
+ *
+ */
+@Entity
+@NamedQueries({ @NamedQuery(name = "Fahrt.findAll", query = "SELECT f FROM Fahrt f"),
+		@NamedQuery(name = "Fahrt.findById", query = "SELECT f FROM Fahrt f WHERE f.fahrtId = :fahrtId") })
+public class Fahrt implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	public static final String FIND_ALL = "Fahrt.findAll";
+
+	public static final String FIND_BY_ID = "Fahrt.findById";
+
+	public Fahrt() {
+
+	}
+
+	@Id
+	public Long fahrtId;
+
+	@NotNull
+	public String departure;
+
+	@NotNull
+	public String destination;
+
+	@Size(min = 10, max = 240)
+	public String description;
+
+	public Long getFahrtId() {
+		return fahrtId;
+	}
+
+	public void setFahrtId(Long fahrtId) {
+		this.fahrtId = fahrtId;
+	}
+
+	public String getDeparture() {
+		return departure;
+	}
+
+	public void setDeparture(String departure) {
+		this.departure = departure;
+	}
+
+	public String getDestination() {
+		return destination;
+	}
+
+	public void setDestination(String destination) {
+		this.destination = destination;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((departure == null) ? 0 : departure.hashCode());
+		result = prime * result + ((description == null) ? 0 : description.hashCode());
+		result = prime * result + ((destination == null) ? 0 : destination.hashCode());
+		result = prime * result + ((fahrtId == null) ? 0 : fahrtId.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Fahrt other = (Fahrt) obj;
+		if (departure == null) {
+			if (other.departure != null)
+				return false;
+		} else if (!departure.equals(other.departure))
+			return false;
+		if (description == null) {
+			if (other.description != null)
+				return false;
+		} else if (!description.equals(other.description))
+			return false;
+		if (destination == null) {
+			if (other.destination != null)
+				return false;
+		} else if (!destination.equals(other.destination))
+			return false;
+		if (fahrtId == null) {
+			if (other.fahrtId != null)
+				return false;
+		} else if (!fahrtId.equals(other.fahrtId))
+			return false;
+		return true;
+	}
+
+}
