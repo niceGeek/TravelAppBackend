ca2ec7decebe806c07df8c9f42baba4be067ca2d
niceGeek <angedesires@gmail.com>
2019-03-14 02:57:03 +0100

    Modified main class

--- src/main/java/de/shingiro/App.java
+++ src/main/java/de/shingiro/App.java
@@ -62,6 +62,66 @@ public class App
     	List<Kind> kinder = em.createNamedQuery("Kind.FIND_ALL", Kind.class).getResultList();
     	kinder.stream().forEach(entity -> System.out.println("Familienname: " + entity.getFamilienname()));
     	*/
+		
+//		@Inject
+//		private FahrerBean bean;
+		
+//		@Inject
+//		private DatenZugriffsManager datenZugriffsManager;
+	//	
+		/**
+		 * @param args
+		 */
+			
+			/*
+			// Creates an instance of driver
+			Fahrer fahrer = new Fahrer();
+			fahrer.setId(150L);
+			fahrer.setVorname("Jamie");
+			fahrer.setNachname("Oliver");
+			fahrer.setTelefonnummer("-156-123456");
+			fahrer.setNachricht("You changed me baby!");
+			fahrer.setAbfahrtzeit("15:20:00");
+			
+			// Creates an instance of the client
+			Fahrgast fahrgast = new Fahrgast();
+			fahrgast.setId(600L);
+			fahrgast.setVorname("Robert");
+			fahrgast.setNachname("Killian");
+			fahrgast.setTelefonnummer("+127-98678");
+			fahrgast.setNachricht("I am looking for a ride");
+			fahrgast.setAbfahrtzeit("15:45:00");
+			*/
+
+			// Obtains an entity manager and a transaction
+			// EntityManagerFactory emf = Persistence.createEntityManagerFactory("travelappdb");
+			// EntityManager em = emf.createEntityManager();
+
+			// Persists the driver to the database
+			// EntityTransaction tx = em.getTransaction();
+			// tx.begin();
+			// em.persist(fahrer);
+			// em.persist(fahrgast);
+			// Response response = Response.ok().entity(Entity.entity(Fahrer.class, "application/json")).build();
+			// System.out.println("Here you are: " + response);
+			// Fahrer driver = em.find(Fahrer.class, 135L);
+			// System.out.println("CHECK: " + driver.getVorname() + " " + driver.getNachname());
+			
+			// Creates JSON object out of response
+			/*
+			Response response = Response.ok().build();
+			System.out.println("ENTITY: " + response);
+			*/
+			// tx.commit();
+
+			// Closes the entity manager and the factory
+			// em.close();
+			// emf.close();
+//			
+//			Fahrer driver = new DatenZugriffsManager().findeFahrer(135L);
+//			
+//			System.out.println("Bitte schoen: " + driver.getVorname() + " " + driver.getNachname());
+			
+		}
 }
 
-}
